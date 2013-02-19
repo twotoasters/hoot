f7ceafab24c31b42fbbbe59b70adf7e07a13114f
James Barr <james@twotoasters.com>
2013-02-19 10:27:44 -0500

    Implement feedback from pull request #10

--- src/com/twotoasters/android/hoot/HootThreadPoolAsyncTask.java
+++ src/com/twotoasters/android/hoot/HootThreadPoolAsyncTask.java
@@ -5,16 +5,15 @@ import android.os.AsyncTask;
 import android.os.Build;
 
 public abstract class HootThreadPoolAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+	
+	private static final boolean IS_SERIAL_EXECUTOR_BY_DEFAULT = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2;
+		
 	@SuppressLint("NewApi")
 	public void executeOnThreadPoolExecutor(Params...params) {
-		if(isSerialExecutorByDefault()) {
+		if(IS_SERIAL_EXECUTOR_BY_DEFAULT) {
 			executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
 		} else {
 			execute(params);
 		}
-	}
-	
-	private static boolean isSerialExecutorByDefault() {
-		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2;
 	}
 }
